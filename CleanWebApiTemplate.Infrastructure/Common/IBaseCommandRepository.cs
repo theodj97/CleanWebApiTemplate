e6c53613370a7e0339797887dc58c331fd359418
Theo Delvas Játiva <theo.delvas@es.gestamp.com>
2025-08-13 11:25:37 +0200

    .

--- CleanWebApiTemplate.Infrastructure/Common/IBaseCommandRepository.cs
+++ CleanWebApiTemplate.Infrastructure/Common/IBaseCommandRepository.cs
@@ -4,18 +4,18 @@ namespace CleanWebApiTemplate.Infrastructure.Common;
 
 public interface IBaseCommandRepository<TEntity> where TEntity : BaseEntity
 {
-    public Task<TEntity> CreateAsync(TEntity entity,
+    Task<TEntity> CreateAsync(TEntity entity,
                                      CancellationToken cancellationToken = default);
-    public Task<bool> BulkInsertAsync(IEnumerable<TEntity> entities,
+    Task<bool> BulkInsertAsync(IEnumerable<TEntity> entities,
                                       CancellationToken cancellationToken = default);
-    public bool BulkInsert(IEnumerable<TEntity> entities);
-    public Task<TEntity> UpdateAsync(TEntity entity,
+    bool BulkInsert(IEnumerable<TEntity> entities);
+    Task<TEntity> UpdateAsync(TEntity entity,
                                      CancellationToken cancellationToken = default);
-    public Task<bool> BulkUpdateAsync(IEnumerable<TEntity> entities,
+    Task<bool> BulkUpdateAsync(IEnumerable<TEntity> entities,
                                       CancellationToken cancellationToken = default);
-    public Task<bool> DeleteAsync(string id,
-                                  CancellationToken cancellationToken = default);
-    public bool Delete(string id);
-    public Task<bool> BulkDeleteAsync(IEnumerable<string> ids,
-                                      CancellationToken cancellationToken = default);
+    Task<bool> DeleteAsync(string id,
+                             CancellationToken cancellationToken = default);
+    bool Delete(string id);
+    Task<bool> BulkDeleteAsync(IEnumerable<string> ids,
+                                CancellationToken cancellationToken = default);
 }
--- CleanWebApiTemplate.Infrastructure/Common/IBaseQueryRepository.cs
+++ CleanWebApiTemplate.Infrastructure/Common/IBaseQueryRepository.cs
@@ -5,13 +5,13 @@ namespace CleanWebApiTemplate.Infrastructure.Common;
 
 public interface IBaseQueryRepository<TEntity> where TEntity : BaseEntity
 {
-    public Task<List<TEntity>> FilterAsync(Expression<Func<TEntity, bool>> expression,
+    Task<List<TEntity>> FilterAsync(Expression<Func<TEntity, bool>> expression,
                                            Expression<Func<TEntity, object>>? orderBy = null,
                                            bool descending = false,
                                            int? pageNumber = null,
                                            int? pageSize = null,
                                            CancellationToken cancellationToken = default);
-    public Task<List<TOutput>> FilterAsync<TOutput>(Expression<Func<TEntity, bool>> expression,
+    Task<List<TOutput>> FilterAsync<TOutput>(Expression<Func<TEntity, bool>> expression,
                                                     Expression<Func<TEntity, TOutput>> selector,
                                                     Expression<Func<TOutput, object>>? orderBy = null,
                                                     bool descending = false,
@@ -19,21 +19,21 @@ public interface IBaseQueryRepository<TEntity> where TEntity : BaseEntity
                                                     int? pageSize = null,
                                                     CancellationToken cancellationToken = default);
 
-    public Task<List<TEntity>> FilterSortAsync(Expression<Func<TEntity, bool>> expression,
+    Task<List<TEntity>> FilterSortAsync(Expression<Func<TEntity, bool>> expression,
                                                IEnumerable<KeyValuePair<string, bool>>? sortProperties = null,
                                                int? pageNumber = null,
                                                int? pageSize = null,
                                                CancellationToken cancellationToken = default);
 
-    public Task<List<TOutput>> FilterSortAsync<TOutput>(Expression<Func<TEntity, bool>> expression,
+    Task<List<TOutput>> FilterSortAsync<TOutput>(Expression<Func<TEntity, bool>> expression,
                                                         Expression<Func<TEntity, TOutput>> selector,
                                                         IEnumerable<KeyValuePair<string, bool>>? sortProperties = null,
                                                         int? pageNumber = null,
                                                         int? pageSize = null,
                                                         CancellationToken cancellationToken = default);
-    public Task<TEntity?> GetByIdAsync(string id,
+    Task<TEntity?> GetByIdAsync(string id,
                                        CancellationToken cancellationToken = default);
-    public Task<TOutput?> GetByIdAsync<TOutput>(string id,
+    Task<TOutput?> GetByIdAsync<TOutput>(string id,
                                                 Expression<Func<TEntity, TOutput>> selector,
                                                 CancellationToken cancellationToken = default);
 }
